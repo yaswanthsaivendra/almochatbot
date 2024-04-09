aeefd0d96b9d6d56282318ba7a015c5da19691ea
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2024-04-10 04:35:02 +0530

    fixed issues

--- server/src/controllers/controllers.js
+++ server/src/controllers/controllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const initChain = require('../utils/utils');
-
+const { HumanMessage, AIMessage } = require("@langchain/core/messages");
 //@desc chat service
 //@route POST /api/chat/
 //@access public
@@ -8,11 +8,15 @@ const chat = asyncHandler(async (req, res) => {
     const body = req.body;
     const question = body.query ?? "what is the order id for shreya order";
     const history = body.history ?? [] 
+    const chat_hist = history.map((message) => {
+        if(message.role == 'assistant') return new AIMessage(message.content);
+        else if(message.role == 'user') return new HumanMessage(message.content);
+    } )
 
     const chain = await initChain();
 
     const response = await chain.invoke({
-        chat_history: [],
+        chat_history: chat_hist,
         input: question,
     });
     res.status(200).json({role : "assistant", content : response.answer});
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 
 
 const app = express();
 
 const port = process.env.PORT || 5000
-
 app.use(express.json());
+app.use(cors());
+app.options('*', cors());
 app.use("/api/chat", require('./routes/routes'));
 app.use(errorHandler);
 
